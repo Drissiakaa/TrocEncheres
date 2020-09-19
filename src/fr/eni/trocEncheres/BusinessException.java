575a20719ac0576ec76186e0d37ebacce6922a5f
drissia <mizanedrissia@gmail.com>
2020-09-19 11:45:07 +0200

    new commit

--- src/fr/eni/trocEncheres/BusinessException.java
+++ src/fr/eni/trocEncheres/BusinessException.java
@@ -1,5 +1,45 @@
 package fr.eni.trocEncheres;
 
-public class BusinessException {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Administrator
+ *
+ *         Cette classe permet de recenser l'ensemble des erreurs (par leur
+ *         code) pouvant survenir lors d'un traitement quel que soit la couche à
+ *         l'origine. c'est la raison pour laquelle elle est placé dans un
+ *         package plus général que bll, bo et dal et servlet mais plutot au
+ *         niveau des package parents (?) mon app
+ */
+public class BusinessException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private List<Integer> listeCodesErreur;
+
+	public BusinessException() {
+		super();
+		this.listeCodesErreur = new ArrayList<>();
+	}
+
+	/**
+	 * 
+	 * @param code
+	 *            Code de l'erreur. Doit avoir un message associé dans un fichier
+	 *            properties.
+	 */
+	public void ajouterErreur(int code) {
+		if (!this.listeCodesErreur.contains(code)) {
+			this.listeCodesErreur.add(code);
+		}
+	}
+
+	public boolean hasErreurs() {
+		return this.listeCodesErreur.size() > 0;
+	}
+
+	public List<Integer> getListeCodesErreur() {
+		return this.listeCodesErreur;
+	}
 
 }
